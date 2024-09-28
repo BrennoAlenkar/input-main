a107f1533905c283454b654624c709b44efe79ff
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-09-28 08:55:51 -0300

    Atualizei

--- js/valida-idade.js
+++ js/valida-idade.js
@@ -1,3 +1,11 @@
 export default function ehMaiorDeIdade(campo) {
     const dataNascimento = new Date(campo.value);
+    ValidityState(dataNascimento);
+}
+
+function validaIdade(data) {
+    const dataAtual = new Date();
+    const dataMais18 = new data(data.getUTCfullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+    return dataAtual >= dataMais18;
 }
